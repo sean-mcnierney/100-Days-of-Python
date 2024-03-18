b5c88738d7307a06073e7ab7cd3806bce295ed0a
sean-mcnierney <80001633+sean-mcnierney@users.noreply.github.com>
2024-03-18 16:53:19 -0400

    Create adventure.py

---
+++ adventure.py
@@ -0,0 +1,44 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+right_left = input("You're at a cross road. Where do you want to go?\nType \"left\" or \"right\"\n")
+if right_left.lower() == "right":
+  print("Fell into a hole! Game Over.")
+else:
+  swim_wait = input("You've come to a lake. There is an island in the middle of the lake.Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n")
+  if swim_wait.lower() == "swim":
+    print("You have been attacked by an angry trout! Game over.")
+  else:
+    door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue.\nType the name of the color to enter that door.\n")
+    if door.lower() == "red":
+      print("It's a room full of fire! Game over.")
+    elif door.lower() == "blue":
+      print("You were eaten by beasts! Game over.")
+    else:
+      print("You found the treasure! You Win!")
+
+#Learned if and elif statements to create a choose your own adventure game. 
